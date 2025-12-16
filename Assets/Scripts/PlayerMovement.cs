ff4beaea0ecdb668c747d82a0a950b02b26ba724
DiazAle5 <youarereta@gmail.com>
2025-12-15 19:59:13 -0500

    Cont

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] int speed = 10;
-    [SerializeField] float minimumDistance;
+    [SerializeField] float minimumDistance = 10;
+    bool movePlayer = false;
+    Vector3 targetPos;
     private void OnEnable()
     {
         MouseRayCastManager.PlayerWantsToMove += MovePlayer;
@@ -16,11 +18,18 @@ public class PlayerMovement : MonoBehaviour
         MouseRayCastManager.PlayerWantsToMove -= MovePlayer;
     }
 
-    void MovePlayer(Vector3 targetPos)
+    private void Update()
     {
-        
+        if(movePlayer == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed);
+        }
+    }
+
+    void MovePlayer(Vector3 targetPosition)
+    {
+        targetPos = targetPosition;
         print("Moving to " + targetPos);
-        while(gameObject.transform.position > Vector3.Distance)
-        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, speed);
+        movePlayer = true;
     }
 }
